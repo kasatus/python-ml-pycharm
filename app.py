edc42fc5af28564b3d6921ab8b00296dea7f6773
skasatki <skasatki@cisco.com>
2017-06-09 13:27:06 -0700

    started learning pandas/numpy with Kaggle introductory learning project

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import numpy as np
-arr = [0, 1, 2]
-arr_n = np.array(arr)
-print(arr_n)
+import pandas
+
+#print(np.array([3, 4, True]))
+train = pandas.read_csv("train.csv")
+test = pandas.read_csv("test.csv")
+# print(train.head())
+#print(train.describe())
+# print(train.shape)
+#print(train["Survived"].value_counts())
+print(train["Survived"].value_counts(normalize = True))
